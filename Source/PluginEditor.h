461bc840ba775d5b954c65b782b3eaf2ebcc555a
espeig200 <espeig200@caledonian.ac.uk>
2025-03-26 15:55:33 +0000

    Base Modules

---
+++ Source/PluginEditor.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class CW2DelayAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    CW2DelayAudioProcessorEditor (CW2DelayAudioProcessor&);
+    ~CW2DelayAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    CW2DelayAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (CW2DelayAudioProcessorEditor)
+};
